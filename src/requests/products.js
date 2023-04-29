0560a3a5a3705f4cceaf63cc275ea9008998e065
DimaSavchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-04-29 16:33:43 +0300

    task3 done

--- src/requests/products.js
+++ src/requests/products.js
@@ -1,35 +1,48 @@
 import { apiInstance } from "../services/api";
 
 
-export async function getProducts() { 
-try {
-    const response = await apiInstance.get("/products");
-    console.log(response);
+export async function getProducts() {
+    try {
+        const response = await apiInstance.get("/products");
+        console.log(response);
 
-    if (response.status === 404) {
-        throw new Error("Not Found");
+        if (response.status === 404) {
+            throw new Error("Not Found");
+        }
+        return response.data.products;
+
+    } catch (error) {
+        console.log(error.message);
     }
-    return response.data.products;
-    
-} catch (error) {
-    console.log(error.message);
-}
-    
+
 }
 
 
 export async function getProductById(id) {
-try {
-  const response = await apiInstance.get(`/products/${id}`);
-  console.log(response);
+    try {
+        const response = await apiInstance.get(`/products/${id}`);
+        console.log(response);
 
-  if (response.status === 404) {
-    throw new Error("Not Found");
-  }
-  return response.data;
-} catch (error) {
-  console.log(error.message);
-}
+        if (response.status === 404) {
+            throw new Error("Not Found");
+        }
+        return response.data;
+    } catch (error) {
+        console.log(error.message);
     }
+}
 
 
+export async function addProduct(data) {
+    try {
+        const response = await apiInstance.post(`/products/add`, data);
+        console.log(response);
+
+        if (response.status === 404) {
+            throw new Error("Not Found");
+        }
+        return response.data;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
