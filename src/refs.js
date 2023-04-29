624216fc72b1310ee39686a9851d90bebfa1c0c2
Liza <lizapikovskaya@gmail.com>
2023-04-29 15:49:39 +0300

    do task 2

--- src/refs.js
+++ src/refs.js
@@ -1,3 +1,5 @@
 export const refs = {
   listProducts: document.querySelector("#allProducts"),
+  singleProductForm: document.querySelector("#singleProductForm"),
+  singleProductDiv: document.querySelector("#singleProduct"),
 };
--- src/requests/products.js
+++ src/requests/products.js
@@ -17,4 +17,19 @@ try {
     
 }
 
-    
+
+export async function getProductById(id) {
+try {
+  const response = await apiInstance.get(`/products/${id}`);
+  console.log(response);
+
+  if (response.status === 404) {
+    throw new Error("Not Found");
+  }
+  return response.data;
+} catch (error) {
+  console.log(error.message);
+}
+    }
+
+
